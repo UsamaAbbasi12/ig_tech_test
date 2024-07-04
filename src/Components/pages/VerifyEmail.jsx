16f0512613dabe691dacba36b2d343a449dcc0ed
usamatahir199 <usamatechnovations@gmail.com>
2024-07-04 17:46:03 +0500

    Verify Email

--- src/Components/pages/VerifyEmail.jsx
+++ src/Components/pages/VerifyEmail.jsx
@@ -16,6 +16,9 @@ const VerifyEmail = () => {
       .then((res) => {
         if (res.data.status === 200) {
           setEmailVirified(true);
+          setTimeout(() => {
+            navigate("/");
+          }, 1000);
         } else {
           toast.error("Can not Verify Email");
         }
@@ -24,37 +27,23 @@ const VerifyEmail = () => {
   const newuseremail = localStorage.getItem("newuseremail");
   return (
     <>
-      <main className="flex flex-col justify-center items-center h-screen border ">
+      <div className="flex flex-col justify-center items-center h-screen  ">
         <div className="">
           {emailVerified ? (
-            <div className="d-flex flex-column justify-content-center">
-              <div className="d-flex flex-column justify-content-center items-center">
-                <div className="pt-5 text-center text-gray-600 d-flex align-items-center gap-2 fs-4">
-                  <h1>
-                    Email{" "}
-                    <span className="fw-bolder text-primary text-decoration-underline">
-                      {newuseremail}
-                    </span>{" "}
-                    Verified Please Login to Continue
-                  </h1>
-                </div>
-                <div className="form-group m-auto mt-5">
-                  <button
-                    onClick={() => navigate("/")}
-                    style={{ width: "200px" }}
-                    className="axil-btn btn-fill-primary btn-fluid btn-primary"
-                    name="submit-btn"
-                  >
-                    Login
-                  </button>
-                </div>
-              </div>
-            </div>
-          ) : (
-            <div className="d-flex flex-column justify-content-center">
-              <h1 className="text-center mb-5">
+            <>
+              <h1>
                 Email{" "}
-                <span className="fw-bolder text-primary text-decoration-underline">
+                <span className="font-bolder text-blue-500 underline">
+                  {newuseremail}
+                </span>{" "}
+                Verified Please Login to Continue
+              </h1>
+            </>
+          ) : (
+            <div>
+              <h1 className="text-center mb-5 text-[30px] ">
+                Email{" "}
+                <span className="font-bold text-blue-500 underline">
                   {newuseremail}
                 </span>{" "}
                 Verification in Progress...{" "}
@@ -62,7 +51,7 @@ const VerifyEmail = () => {
             </div>
           )}
         </div>
-      </main>
+      </div>
     </>
   );
 };
